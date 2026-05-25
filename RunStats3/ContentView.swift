6f69105d23f6bee05c7a2536d42ffca2254a26f1
Ron Somerville <rsomervillejr@gmail.com>
2026-05-25 11:20:25 -0400

    Initial Commit

---
+++ RunStats3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RunStats3
+//
+//  Created by Ronald Somerville on 5/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RunStats3/RunStats3App.swift
@@ -0,0 +1,17 @@
+//
+//  RunStats3App.swift
+//  RunStats3
+//
+//  Created by Ronald Somerville on 5/25/26.
+//
+
+import SwiftUI
+
+@main
+struct RunStats3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
